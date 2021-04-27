76e15de7160abff75e0f10ae2688bde75f49db9d
Carla Polanco <carla.polanco@usach.cl>
2021-04-27 18:37:29 -0400

    Estudio del lenguaje de programacion, creacion del proyecto en github y extraccion inicial de los TDA

---
+++ Proyecto1.rkt
@@ -0,0 +1,82 @@
+#lang racket
+
+;PRIMER TDA (CUENTA)
+
+;REPRESENTACION
+;CREAR PUBLICACION (Registrar)
+
+
+;PERTENENCIA
+;Comprobar si la publicacion es correcta
+;Comprobar que es una publicaicon valida
+
+;SELECTORES
+
+
+;MODIFICADORES
+;Eliminar la publicacion
+
+;OTRAS OPERACIONES
+;Responder/compartir/reaccionar publicaciones
+;Iniciar seccion
+;Añadir/seguir amigo/empresa
+;Enviar mensaje privado
+
+
+;SEGUNDO TDA (ACTORES USUARIOS)
+
+;REPRESENTACION
+;Crear una cuenta
+
+;PERTENENCIA
+;Existe la cuenta
+;Es valida la cuenta
+
+;SELECTORES
+
+;MODIFICADORES
+;Eliminar cuenta
+;cambiar contraseña
+
+;OTRAS OPERACIONES
+
+
+; TERCER TDA (PUBLICACIONES)
+
+;REPRESENTACION
+;crear publicacion
+
+;PERTENENCIA
+;Es valida
+
+;SELECTORES
+
+;MODIFICADORES
+;Eliminar publicacion
+;Responder una publicacion
+;Editar una reaccion
+
+;OTRAS OPERACIONES
+;Compartir publicacion
+
+
+;CUARTO TDA (REACCION)
+;REPRESENTACION
+;crear reaccion (tipos de reaccion)
+
+;PERTENENCIA
+;Es valida
+
+;SELECTORES
+
+;MODIFICADORES
+;Eliminar reaccion
+
+;OTRAS OPERACIONES
+;Compartir reaccion
+
+
+
+
+
+
